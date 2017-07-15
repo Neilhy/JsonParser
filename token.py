6c560414aa739ac2fa2b2842e826560abf25c51b
Neilhy <neilhyzou@gmail.com>
2017-07-15 13:33:07 +0800

    add josn's token file

---
+++ token.py
@@ -0,0 +1,63 @@
+# -*- coding:utf-8 -*-
+
+
+class TokenType:
+    """
+    object
+        {}
+        { members }
+    members
+        pair
+        pair , members
+    pair
+        string : value
+    array
+        []
+        [ elements ]
+    elements
+        value
+        value , elements
+    value
+        string
+        number
+        object
+        array
+        true
+        false
+        null
+    string
+        ""
+        " chars "
+    chars
+        char
+        char chars
+    char
+        any-Unicode-character-
+        except-"-or-\-or-
+        control-character
+        \"
+        \\
+        \/
+        \b
+        \f
+        \n
+        \r
+        \t
+        \u four-hex-digits
+    number
+        int
+        int frac
+        int exp
+        int frac exp
+    """
+    START_OBJ = 1
+    END_OBJ = 2
+    START_ARRAY = 3
+    END_ARRAY = 4
+    NULL = 5
+    NUMBER = 6
+    STRING = 7
+    BOOLEAN = 8
+    COLON = 9
+    COMMA = 10
+    END_DOC = 11

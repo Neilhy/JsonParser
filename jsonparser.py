d25e000dbd4cfa8b62d30d1dfe327abbc5c2b5c1
Neilhy <neilhyzou@gmail.com>
2017-07-15 13:34:41 +0800

    add jsonparser class

---
+++ jsonparser.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+
+
+class JsonParser(object):
+    """Parse json using Python"""
+
+    def __init__(self):
+        self._data = {}
+
+    def loads(self, s):
+        """load JSON from string"""
+        pass

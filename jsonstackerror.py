5243f875b469ca1dc0b4a5c4e82fff7e84cbe9c8
Neilhy <neilhyzou@gmail.com>
2017-07-17 15:38:05 +0800

    add file:jsonstackerror.py

---
+++ jsonstackerror.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+# -*- coding:utf -*-
+
+from jsonerror import JsonError
+
+
+class JsonStackError(JsonError):
+    """
+    Exception raised for json-stack's inside error.
+
+    Attributes:
+        where -- where the error occurred
+        message -- explanation of the error
+    """
+
+    def __init__(self, message, where=None):
+        self.where = where
+        self.message = message
+
+    def __str__(self):
+        if self.where is None:
+            return self.message
+        else:
+            return str(self.where) + " - " + str(self.message)

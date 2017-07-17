d88280a15245fc31d9bb5368689b819e5a0ee48e
Neilhy <neilhyzou@gmail.com>
2017-07-17 21:41:23 +0800

    add charreader,filereader,stringreader files

---
+++ charreader.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+# -*- coding:utf -*-
+
+
+import eoferror
+
+
+class CharReader(object):
+    BUFFER_SIZE = 10
+
+    def __init__(self, reader):
+        self.reader = reader
+        self.buffer = []
+        self.had_read = 0
+        self.pos = 0
+        self.buffer_size = 0
+
+    def has_more_char(self):
+        if self.pos < self.buffer_size:
+            return True
+        self.read_into_buffer(None)
+        return self.pos < self.buffer_size
+
+    def read_into_buffer(self, eof_msg):
+        string = self.reader.read(self.BUFFER_SIZE)
+        self.buffer = list(string)
+        if not self.buffer:
+            if eof_msg is not None:
+                raise eoferror.JsonEOFError(eof_msg, "In CharReader")
+            return
+        self.pos = 0
+        self.buffer_size = len(self.buffer)
+        self.had_read += self.buffer_size
+
+    def read_next_char(self):
+        if self.pos == self.buffer_size:
+            self.read_into_buffer("EOF of the file.")
+        c = self.buffer[self.pos]
+        self.pos += 1
+        return c
+
+    def read_top_char(self):
+        if self.pos == self.buffer_size:
+            self.read_into_buffer("EOF of the file.")
+        return self.buffer[self.pos]
---
+++ filereader.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# -*- coding:utf -*-
+
+
+class FileReader(object):
+    def __init__(self, file_name):
+        self.file = open(file_name)
+
+    def read(self, buf_size):
+        return self.file.read(buf_size)
+
+#
+# fr = FileReader("E:/json.txt")
+# while True:
+#     str1 = fr.read(10)
+#     if not str1:
+#         break
+#     print str1
